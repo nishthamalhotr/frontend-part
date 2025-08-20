e4fde35cbb0d34276e150739c2eea7ac8a180350
jenny42005 <166281034+nishthamalhotr@users.noreply.github.com>
2025-08-20 22:19:16 +0530

    Add files via upload

---
+++ New folder/orders.js
@@ -0,0 +1,40 @@
+// orders.js
+
+const Orders = [
+    {
+        productName: '2MP global shutter camera',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: '2MP global shutter camera',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Delivered'
+    }
+];
+
+// Make available globally if using <script> tags
+window.Orders = Orders;
+
+// If using ES Modules, uncomment below:
+// export default Orders;
